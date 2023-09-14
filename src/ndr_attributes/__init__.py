268ab0cc84d9789db93354065149ff6484f2cce6
Jayant Rao <jayantrao88@gmail.com>
2023-09-13 21:36:31 -0400

    feat: add easier import of methods in init.py and include tests and changelog in .toml file

--- src/ndr_attributes/__init__.py
+++ src/ndr_attributes/__init__.py
@@ -1,3 +1,7 @@
 # read version from installed package
 from importlib.metadata import version
 __version__ = version("ndr_attributes")
+
+# populate package namespace
+from pycounts.pycounts import count_words
+from pycounts.plotting import plot_words
